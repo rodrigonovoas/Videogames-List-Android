0d01e763e3a3ad05d09603321475b7e769270a1e
Rodrigo N <rodrigonovoa97s@gmail.com>
2022-04-07 21:22:25 +0200

    initalizing project

---
+++ app/src/main/java/app/rodrigonovoa/myvideogameslist/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.rodrigonovoa.myvideogameslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
