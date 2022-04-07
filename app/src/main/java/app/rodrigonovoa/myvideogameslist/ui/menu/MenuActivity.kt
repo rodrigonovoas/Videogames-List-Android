971b241b9dfdf2e0e7a32f7003f47ea24d2581ff
Rodrigo N <rodrigonovoa97s@gmail.com>
2022-04-07 21:43:20 +0200

    creating project packages and setting menu: the core activity of the project

--- app/src/main/java/app/rodrigonovoa/myvideogameslist/ui/menu/MenuActivity.kt
+++ app/src/main/java/app/rodrigonovoa/myvideogameslist/ui/menu/MenuActivity.kt
@@ -1,9 +1,10 @@
-package app.rodrigonovoa.myvideogameslist
+package app.rodrigonovoa.myvideogameslist.ui.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.rodrigonovoa.myvideogameslist.R
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
