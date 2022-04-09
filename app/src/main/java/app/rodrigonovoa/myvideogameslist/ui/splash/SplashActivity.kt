c23484a21f229b2d1bb36db2c2c495544b3732c6
Rodrigo N <rodrigonovoa97s@gmail.com>
2022-04-09 10:36:18 +0200

    adding view binding and setup basic animation

--- app/src/main/java/app/rodrigonovoa/myvideogameslist/ui/splash/SplashActivity.kt
+++ app/src/main/java/app/rodrigonovoa/myvideogameslist/ui/splash/SplashActivity.kt
@@ -1,9 +1,14 @@
 package app.rodrigonovoa.myvideogameslist.ui.splash
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import app.rodrigonovoa.myvideogameslist.R
+import app.rodrigonovoa.myvideogameslist.databinding.ActivitySplashBinding
+import app.rodrigonovoa.myvideogameslist.ui.menu.MenuActivity
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.input.input
 
@@ -13,21 +18,54 @@ import com.afollestad.materialdialogs.input.input
  */
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val TIME_SPLASH_SCREEN = 2500L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // dialog to ask for username
-        MaterialDialog(this).show {
+        val context = this@SplashActivity
+        // dialog to store username
+        startAnimation(context)
+        setTimerAndExit(context)
+    }
+
+    private fun createUserDialog(context: Context){
+        MaterialDialog(context).show {
             title(R.string.username_dialog_title)
             input(allowEmpty = false) { dialog, text ->
-               Toast.makeText(context,"Prueba",Toast.LENGTH_SHORT).show()
+                // TODO: store username
+
+                // store and move to the main activity
+                openMainMenu(this@SplashActivity)
             }
             positiveButton(R.string.username_dialog_button)
 
-            // prevent cancellation
+            // prevent dialog cancellation
             cancelable(false)
             cancelOnTouchOutside(false)
         }
+    }
+    private fun setTimerAndExit(context: Context) {
+        val handler = Handler()
+        handler.postDelayed({
+            // TODO: check if user is stored; then move to the main activity
+            createUserDialog(context)
+            //openMainMenu(context)
+            //this.finish()
+        }, TIME_SPLASH_SCREEN)
+    }
+
+    private fun openMainMenu(context: Context){
+        val intent = Intent(context, MenuActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startAnimation(context: Context){
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.root.startAnimation(animation)
     }
 }
